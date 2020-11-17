c8993662e55bb86cb047dc87f19d8cceacd8012e
Ayush Kamat <akamat@berkeley.edu>
2020-11-16 18:13:32 -0800

    added generative trainer

---
+++ configs/generative_toy_config.py
@@ -0,0 +1,69 @@
+from dotmap import DotMap
+from trainers.adversarial_trainer import AdversarialTrainer
+from data.adversarial_toy_gauss_data import AdversarialToyGauss
+from data.toy_gauss_data import ToyGauss
+from models.mlp import mlp
+from models.generator import generator
+from torch import nn
+from torch.optim import Adam
+import torch
+
+config = DotMap()
+config.seed = 1
+
+config.trainer = AdversarialTrainer
+config.tp.epochs = 16
+config.tp.log_train_every = 50
+config.tp.train_gen_every = 2
+config.tp.generator_loss = lambda input, target : -nn.KLDivLoss(log_target=True, reduction='batchmean')(input, target)
+config.tp.student_loss = nn.KLDivLoss(log_target=True, reduction='batchmean')
+config.tp.test_loss = nn.NLLLoss() 
+config.tp.use_gpu = False
+config.tp.device = torch.device('cuda') if config.tp.use_gpu else torch.device('cpu')
+
+config.opt = Adam
+config.op.lr = 1e-3
+
+config.dataset = AdversarialToyGauss
+config.dp.device = config.tp.device
+config.dp.seed = config.seed
+config.dp.gauss_dim = 3
+config.dp.num_classes = 3
+config.dp.batch_size = 128
+config.dp.num_samples = 100000
+
+config.test_dataset = ToyGauss
+config.tdp.device = config.tp.device
+config.tdp.seed = config.seed
+config.tdp.gauss_dim = 3
+config.tdp.num_classes = 3
+config.tdp.batch_size = 128
+config.tdp.num_samples = 1000
+
+config.generator.model = generator
+config.generator.device = config.tp.device
+config.generator.noise_dim = 100
+config.generator.noise_mean = 0
+config.generator.noise_std = 1
+config.generator.input_size = config.generator.noise_dim
+config.generator.hidden_sizes = [8, 32, 16]
+config.generator.output_size = config.dp.gauss_dim
+config.generator.activation = nn.ReLU()
+config.generator.output_activation= nn.Identity()
+
+config.teacher.model = mlp
+config.teacher.device = config.tp.device
+config.teacher.checkpoint = 'logs/toy_config@1605515479/weights/model.pth'
+config.teacher.input_size = config.dp.gauss_dim
+config.teacher.hidden_sizes = [8, 32, 16]
+config.teacher.output_size = config.dp.num_classes
+config.teacher.activation = nn.ReLU()
+config.teacher.output_activation= nn.LogSoftmax(dim=1)
+
+config.student.model = mlp
+config.student.device = config.tp.device
+config.student.input_size = config.dp.gauss_dim
+config.student.hidden_sizes = [4, 8, 4]
+config.student.output_size = config.dp.num_classes
+config.student.activation = nn.ReLU()
+config.student.output_activation= nn.LogSoftmax(dim=1)
---
+++ trainers/generative_trainer.py
@@ -0,0 +1,131 @@
+from trainers.trainer import Trainer
+from collections import defaultdict 
+from torch.utils.data import DataLoader
+import torch
+from tqdm import tqdm
+import os
+torch.autograd.set_detect_anomaly(True)
+
+class GenerativeTrainer(Trainer):
+    """
+    Teacher is a trained model whose weights are loaded from a checkpoint.
+    Student is a new model, potentially of a different architecture.
+    The student will learn by matching its prediction to what the teacher predicts for a generated input
+    The generator will have a loss of its own, which will hopefully encourage more structure to the generated inputs
+    """
+    def __init__(self, config):
+        super().__init__(config)
+        self.teacher = self.c.teacher.model(self.c.teacher)
+        self.student = self.c.student.model(self.c.student)
+        self.generator = self.c.generator.model(self.c.generator)
+        self.teacher.load_state_dict(torch.load(self.c.teacher.checkpoint))
+        self.c.dp.teacher = self.teacher
+        self.c.dp.generator = self.generator
+        self.dataset = self.c.dataset(self.c.dp)
+        self.dataloader = DataLoader(self.dataset, batch_size=self.c.dp.batch_size)
+        self.test_dataset = self.c.test_dataset(self.c.tdp, mode='test')
+        self.test_dataloader = DataLoader(self.test_dataset, batch_size=self.c.tdp.batch_size)
+        self.stud_opt = self.c.opt(self.student.parameters(), lr = self.c.op.lr)
+        self.gen_opt = self.c.opt(self.generator.parameters(), lr = self.c.op.lr)
+
+    def train(self, batch):
+        # generated_input, teacher_output = batch
+        # student_output = self.student(generated_input)
+
+        # generator_loss = 0.5 * (self.c.tp.generator_loss(teacher_output, student_output) + self.c.tp.generator_loss(student_output, teacher_output))
+        # student_loss = self.c.tp.student_loss(student_output, teacher_output.detach())
+
+        # result = {'train/student_loss': student_loss,
+        #           'train/generator_loss': generator_loss}
+
+        # return result
+
+        label = batch # y
+        generated_image = self.generator(label) # x
+
+        teacher_label = self.teacher(generated_image)
+        student_label = self.student(generated_image)
+
+        gen_loss = 0.5 * (self.c.tp.generator_loss(teacher_label, label) + self.c.tp.generator_loss(label, teacher_label)) 
+        stu_loss = self.c.tp.student_loss(student_label, teacher_label.detach())
+
+        result = {'train/generator_loss': gen_loss, 'train/student_loss': stu_loss}
+
+        return result
+
+
+
+
+    def test(self):
+        # result = defaultdict(lambda: 0)
+        # with torch.no_grad():
+        #     for i, batch in enumerate(self.test_dataloader):
+        #         x,y = batch
+        #         pred = self.student(x)
+        #         loss = self.c.tp.test_loss(pred, y)
+        #         result['test/loss'] += loss
+        #         result['test/acc'] += (torch.argmax(pred, dim=1) == y).float().mean()
+
+        # for k in result:
+        #     result[k] = result[k]/(i+1)
+
+        # self.log(result)
+        # return result
+
+        result = defaultdict(lambda: 0)
+        with torch.no_grad():
+            for i, batch in enumerate(self.test_dataloader):
+                x, y = batch
+                gen_x = self.generator(y)
+                pred_student = self.student(gen_x)
+                pred_teacher = self.teacher(gen_x)
+                loss = self.c.tp.test_loss(pred_student, pred_teacher)
+                result['test/loss'] += loss
+
+        for k in result:
+            result[k] = result[k] / (i+1)
+
+        self.log(result)
+        return result
+
+
+
+    def zero_grads(self):
+        self.teacher.zero_grad()
+        self.generator.zero_grad()
+        self.student.zero_grad()
+
+    def run(self):
+        self.total_iterations = len(self.dataloader)*self.c.tp.epochs
+        pbar = tqdm(total=self.total_iterations)
+        
+        self.test()
+        generator_loss = torch.tensor(0)
+        student_loss = torch.tensor(0)
+        for epoch in range(self.c.tp.epochs):
+            for ind, batch in enumerate(self.dataloader):
+                self.zero_grads()
+                if ind % self.c.tp.train_gen_every == 0:
+                    result = self.train(batch)
+                    generator_loss = result['train/generator_loss']
+                    generator_loss.backward()
+                    self.gen_opt.step()
+                else:
+                    result = self.train(batch)
+                    student_loss = result['train/student_loss']
+                    student_loss.backward()
+                    self.stud_opt.step()
+
+                if self.iteration % self.c.tp.log_train_every == 0:
+                    self.log(result)
+                self.iteration += 1
+                pbar.set_description("Epoch {}/{} | Gen Loss {:.2e} Student Loss {:.2e}".format(epoch+1, self.c.tp.epochs, generator_loss.item(), student_loss.item()))
+                pbar.update(1)
+            self.test()
+
+        self.save()
+    
+    def save(self):
+        torch.save(self.generator.state_dict(), os.path.join(self.c.weights_path, '{}.pth'.format('generator')))
+        torch.save(self.student.state_dict(), os.path.join(self.c.weights_path, '{}.pth'.format('student')))
+        torch.save(self.teacher.state_dict(), os.path.join(self.c.weights_path, '{}.pth'.format('teacher')))
